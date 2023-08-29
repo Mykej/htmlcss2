9543ca8611ee28070ae5a2371a96c2dd5897a661
Mykej <mekrfz23_23@hotmail.com>
2023-08-28 21:32:43 -0500

    PHP notes on sending secure contact data via php variables and built-in methods.

--- website/process.php
+++ website/process.php
@@ -1,16 +1,17 @@
 <?php ob_start();
 
 
-
+  // Catches the submit button when it is clicked in contact.html and fills in the variables with the data in the contact form.
 	if(isset($_POST['submit'])) {
 		
-	$to = "edwin@codingfaculty.com";
+	$to = "bigpicfoc90@gmail.com";
 	$subject = $_POST['subject'];
 	$email = $_POST['email'];
 	$txt = $_POST['message'];
 	$headers = "From: " .$email . "\r\n" .
 	"CC: somebodyelse@example.com";
 	
+  // built in method that sends variables to specific target.
 	mail($to,$subject,$txt,$headers);
 	
 	header("Location: contact.html");
